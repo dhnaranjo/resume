a048119a56d8053253af1b349a72a9154751f13a
Desmond Naranjo <dhnaranjo@gmail.com>
2018-02-05 12:19:15 -0600

    Updates

---
+++ path---index-ea461681abfa49a665c4.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{467:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Wow buddy!"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-ea461681abfa49a665c4.js.map
